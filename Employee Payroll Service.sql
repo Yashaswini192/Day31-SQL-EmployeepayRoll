c94f439c22fae52d9c833485f8eb04e9174459ca
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-03-16 20:55:43 +0530

    create employee payroll data in the Database as a part of crud operation

--- Employee Payroll Service.sql
+++ Employee Payroll Service.sql
@@ -1,2 +1,11 @@
 create database payroll_service;
 use payroll_service;
+create table employee_payroll(
+								Id int auto_increment,
+                                name varchar(30),
+                                salary int ,
+                                start_date int,
+								primary key(Id)
+                                );
+select * from employee_payroll;
+insert into employee_payroll(Id,name,salary,start_date) values(1,"Chitti",9000,19052019);
