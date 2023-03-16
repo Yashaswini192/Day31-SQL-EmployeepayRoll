e28e3218419a694a9dc753b67cddfed69a0a759e
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-03-16 18:21:01 +0530

    create employee payroll table in payroll service database

--- Employee Payroll Service.sql
+++ Employee Payroll Service.sql
@@ -1,2 +1,10 @@
 create database payroll_service;
 use payroll_service;
+create table employee_payroll(
+								Id int auto_increment ,
+                                name varchar(30),
+                                salary int ,
+                                start_date int,
+                                primary key(Id)
+                                )
+select * from employee_payroll;
